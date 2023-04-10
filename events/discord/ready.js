43e4bb625f39b81f9f78a7d46820472e8fff4170
Electrix <117116279+electrixOnYT@users.noreply.github.com>
2023-04-10 07:56:47 +0300

    Add files via upload

---
+++ events/discord/ready.js
@@ -0,0 +1,22 @@
+const register = require('../../utils/slashsync');
+module.exports = async (client) => {
+  await register(client, client.register_arr.map((command) => ({
+    name: command.name,
+    description: command.description,
+    options: command.options,
+    type: 'CHAT_INPUT'
+  })), {
+    debug: true
+  });
+
+
+  console.log(`[ / | Slash Command ] - ✅ Loaded all slash commands!`)
+  let invite = `https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=applications.commands%20bot`;
+  console.log(`[STATUS] ${client.user.tag} is now online!\n[INFO] Bot by Electrix#0001 https://www.youtube.com/Electrixxx\n[Invite Link] ${invite}`);
+  const activities = [`discord.gg/axle`];
+  setInterval(() => {
+    let activity = activities[Math.floor(Math.random() * activities.length)];
+    client.user.setActivity(activity, { type: "WATCHING" });
+  }, 20000);
+
+};
